f5909b859cb4a175fe814c1838cba5306238c3bb
Apel Mahmud <apelmahmud.dev@gmail.com>
2024-05-28 22:10:31 +0600

    [refactor] Techvillage chat widget

--- chatbot-sdk.js
+++ chatbot-sdk.js
@@ -5,13 +5,13 @@
 	const fabBgColor = scriptTag.getAttribute("data-fab-bg-color") || "#24E186CC";
 	// const fabIcon = scriptTag.getAttribute("data-fab-icon");
 	const fabIcon = "https://i.ibb.co/CthkwCV/img-robot-face.png";
-	const fabCloseIcon = "https://i.ibb.co/CthkwCV/img-robot-face.png";
+	const fabCloseIcon = "";
 	const iframeSrc = scriptTag.getAttribute("data-iframe-src") || "";
 	const iframeWidth = scriptTag.getAttribute("data-iframe-width") || 320;
 	const iframeHeight = scriptTag.getAttribute("data-iframe-height") || 532;
 
 	// Variable to check if the chat widget is open
-	let isOpen = false;
+	let isOpen = true;
 
 	// Check if the iframe source is set
 	if (!iframeSrc) {
@@ -61,7 +61,7 @@
             position: fixed;
             bottom: 20px;
             right: 21px;
-            z-index: 1000;
+            z-index: 1500;
         }
         .techvill-widget-fab-button {
             display: flex;
@@ -73,23 +73,26 @@
             color: #fff;
             border: none;
             cursor: pointer;
+			background-color: #24E186CC;
             box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
+			transition: all 0.1s ease-in-out;
         }
         .techvill-widget-fab-icon {
             height: 47px;
             width: 47px;
         }
         .techvill-widget-chat-modal {
-            display: none;
+            visibility: hidden;
+			opacity: 0;
             position: fixed;
-            bottom: 110px;
+            bottom: 70px;
             right: 21px;
             background-color: #fff;
             z-index: 1000;
             border-radius: 10px;
             overflow: hidden;
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3); 
-            transition: all 0.3s;
+            transition: all 0.2s ease-in-out;
         }
         .techvill-widget-notfound-iframe {
             min-height: 532px;
@@ -112,53 +115,32 @@
 
 	// Variables declaration
 	const fabButton = document.getElementById("techvill-widget-fab");
-
-	// Add JavaScript interactivity
-	const fabImg = document.createElement("img");
-	fabImg.className = "techvill-widget-fab-icon";
-	fabImg.id = "techvill-widget-icon";
-	fabImg.src = fabIcon;
-	fabImg.style.objectFit = "contain";
-	fabImg.alt = "open";
-	fabButton.appendChild(fabImg);
-
-	// Initialize default icon
-	if (!fabIcon && !isOpen) {
-		fabButton.innerHTML = `
-		<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 46 46" fill="none">
-		<path fill-rule="evenodd" clip-rule="evenodd" d="M12.3477 3.83331H33.661C38.5162 3.99995 42.3217 8.06263 42.171 12.9183V25.415C42.2432 27.7601 41.3777 30.0372 39.7661 31.7423C38.1545 33.4474 35.9298 34.4399 33.5844 34.5H15.3377C14.075 34.5261 12.8775 35.066 12.0219 35.995L7.47937 40.8441C7.09452 41.2639 6.55376 41.5065 5.98437 41.515C5.39629 41.5 4.83861 41.2506 4.43543 40.8222C4.03226 40.3938 3.81705 39.822 3.8377 39.2341V12.9183C3.68699 8.06263 7.49254 3.99995 12.3477 3.83331ZM33.661 31.625C36.9288 31.4596 39.448 28.6834 39.296 25.415V12.9183C39.448 9.64986 36.9288 6.87366 33.661 6.70831H12.3477C9.07989 6.87366 6.56075 9.64986 6.7127 12.9183V37.4516L9.83686 34.04C11.2656 32.5202 13.2519 31.6481 15.3377 31.625H33.661Z" fill="#FFA500"/>
-		<path d="M15.8169 16.7708H25.4002C26.1941 16.7708 26.8377 16.1272 26.8377 15.3333C26.8377 14.5394 26.1941 13.8958 25.4002 13.8958H15.8169C15.023 13.8958 14.3794 14.5394 14.3794 15.3333C14.3794 16.1272 15.023 16.7708 15.8169 16.7708Z" fill="#FFA500"/>
-		<path d="M30.1919 21.5625H15.8169C15.023 21.5625 14.3794 22.2061 14.3794 23C14.3794 23.7939 15.023 24.4375 15.8169 24.4375H30.1919C30.9858 24.4375 31.6294 23.7939 31.6294 23C31.6294 22.2061 30.9858 21.5625 30.1919 21.5625Z" fill="#FFA500"/>
-		</svg>`;
-	}
+	fabButton.innerHTML = `<img src="${fabIcon}" alt="open"/>`;
 
 	fabButton.addEventListener("click", function () {
-		if (!isOpen) {
-			document.getElementById("techvill-widget-chat-modal").style.display =
-				"block";
-			if (!fabCloseIcon) {
-				fabButton.innerHTML = `
-				<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none">
-					<path d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18" stroke="#FCCA19" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-				</svg>`;
-			} else {
-				fabImg.src = fabCloseIcon;
-				fabImg.alt = "Close";
-			}
+		if (isOpen) {
+			document.getElementById("techvill-widget-chat-modal").style.visibility =
+				"visible";
+			document.getElementById("techvill-widget-chat-modal").style.bottom =
+				"110px";
+			document.getElementById("techvill-widget-chat-modal").style.opacity = "1";
+
+			fabButton.innerHTML = `<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path fill-rule="evenodd" clip-rule="evenodd" d="M0.536971 0.536971C1.25293 -0.17899 2.41373 -0.17899 3.1297 0.536971L11 8.40728L18.8703 0.536971C19.5863 -0.17899 20.7471 -0.17899 21.463 0.536971C22.179 1.25293 22.179 2.41373 21.463 3.1297L13.5927 11L21.463 18.8703C22.179 19.5863 22.179 20.7471 21.463 21.463C20.7471 22.179 19.5863 22.179 18.8703 21.463L11 13.5927L3.1297 21.463C2.41373 22.179 1.25293 22.179 0.536971 21.463C-0.17899 20.7471 -0.17899 19.5863 0.536971 18.8703L8.40728 11L0.536971 3.1297C-0.17899 2.41373 -0.17899 1.25293 0.536971 0.536971Z" fill="white"/>
+			</svg>`;
+
+			// fabButton svg show zoom in animation
+			fabButton.style.transform = "rotate(90deg)";
+			fabButton.style.transition = "all 0.2s ease-in-out";
 		} else {
-			document.getElementById("techvill-widget-chat-modal").style.display =
-				"none";
-			if (!fabIcon) {
-				fabButton.innerHTML = `
-				<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 46 46" fill="none">
-					<path fill-rule="evenodd" clip-rule="evenodd" d="M12.3477 3.83331H33.661C38.5162 3.99995 42.3217 8.06263 42.171 12.9183V25.415C42.2432 27.7601 41.3777 30.0372 39.7661 31.7423C38.1545 33.4474 35.9298 34.4399 33.5844 34.5H15.3377C14.075 34.5261 12.8775 35.066 12.0219 35.995L7.47937 40.8441C7.09452 41.2639 6.55376 41.5065 5.98437 41.515C5.39629 41.5 4.83861 41.2506 4.43543 40.8222C4.03226 40.3938 3.81705 39.822 3.8377 39.2341V12.9183C3.68699 8.06263 7.49254 3.99995 12.3477 3.83331ZM33.661 31.625C36.9288 31.4596 39.448 28.6834 39.296 25.415V12.9183C39.448 9.64986 36.9288 6.87366 33.661 6.70831H12.3477C9.07989 6.87366 6.56075 9.64986 6.7127 12.9183V37.4516L9.83686 34.04C11.2656 32.5202 13.2519 31.6481 15.3377 31.625H33.661Z" fill="#FFA500"/>
-					<path d="M15.8169 16.7708H25.4002C26.1941 16.7708 26.8377 16.1272 26.8377 15.3333C26.8377 14.5394 26.1941 13.8958 25.4002 13.8958H15.8169C15.023 13.8958 14.3794 14.5394 14.3794 15.3333C14.3794 16.1272 15.023 16.7708 15.8169 16.7708Z" fill="#FFA500"/>
-					<path d="M30.1919 21.5625H15.8169C15.023 21.5625 14.3794 22.2061 14.3794 23C14.3794 23.7939 15.023 24.4375 15.8169 24.4375H30.1919C30.9858 24.4375 31.6294 23.7939 31.6294 23C31.6294 22.2061 30.9858 21.5625 30.1919 21.5625Z" fill="#FFA500"/>
-				</svg>`;
-			} else {
-				fabImg.src = fabIcon;
-				fabImg.alt = "Open";
-			}
+			fabButton.innerHTML = `<img id="chat-icon" src="${fabIcon}" alt="open"/>`;
+			document.getElementById("techvill-widget-chat-modal").style.visibility =
+				"hidden";
+			document.getElementById("techvill-widget-chat-modal").style.bottom =
+				"70px";
+			document.getElementById("techvill-widget-chat-modal").style.opacity = "0";
+			fabButton.style.transform = "rotate(0deg)";
+			fabButton.style.transition = "all 0.08s ease-in-out";
 		}
 		isOpen = !isOpen;
 	});
